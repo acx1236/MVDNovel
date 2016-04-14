1fbaafbb3a45e199f68648613cd07d980a1d59ab
Ancx <acx1236@163.com>
2016-04-14 13:55:41 +0800

    MVD小说

---
+++ app/src/main/java/com/ancx/mvdnovel/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.ancx.mvdnovel;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 欢迎界面
+ */
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
