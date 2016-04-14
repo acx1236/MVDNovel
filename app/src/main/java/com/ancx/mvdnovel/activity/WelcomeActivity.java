428ad330fbbc050c311ced4ffc04f4f480071161
Ancx <acx1236@163.com>
2016-04-14 14:01:24 +0800

    MVD小说

---
+++ app/src/main/java/com/ancx/mvdnovel/activity/WelcomeActivity.java
@@ -0,0 +1,32 @@
+package com.ancx.mvdnovel.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ancx.mvdnovel.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * 欢迎界面
+ */
+public class WelcomeActivity extends AppCompatActivity {
+
+    private Timer timer = new Timer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 1200);
+    }
+}
