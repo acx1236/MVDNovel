3e535e1fd8ee8b5590a0853c72ee79daba025fd3
Ancx <acx1236@163.com>
2016-04-27 17:52:13 +0800

    优化加载图片卡顿

--- app/src/main/java/com/ancx/mvdnovel/util/ImageLoader.java
+++ app/src/main/java/com/ancx/mvdnovel/util/ImageLoader.java
@@ -7,17 +7,22 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by Ancx on 16/2/25.
  */
 public class ImageLoader {
+
+    private final static ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public static String getKey(String imagePath) {
         return MD5Util.GetMD5Code(imagePath);
     }
 
     // 显示图片
-    public static void display(String imagePath, ImageView mImageView, int defaultResource, int errorResource, int maxWidth, int maxHeight) {
+    public static void display(final String imagePath, final ImageView mImageView, int defaultResource, final int errorResource, final int maxWidth, final int maxHeight) {
         if (MemoryUtil.getMemory().get(getKey(imagePath)) != null) {
             mImageView.setImageBitmap(MemoryUtil.getMemory().get(getKey(imagePath)));
         } else {
@@ -26,30 +31,40 @@ public class ImageLoader {
             mImageView.setTag(imagePath);
             mImageView.setImageResource(defaultResource);
             // sd卡中查找
-            Bitmap bitmap = BitmapUtil.readBitMap(MemoryUtil.getSaveBitmapPath(getKey(imagePath)), maxWidth, maxHeight);
-            if (bitmap != null) {
-                if (imagePath.equals(mImageView.getTag())) {
-                    mImageView.setImageBitmap(bitmap);
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    Bitmap bitmap = BitmapUtil.readBitMap(MemoryUtil.getSaveBitmapPath(getKey(imagePath)), maxWidth, maxHeight);
+                    if (bitmap != null) {
+                        if (imagePath.equals(mImageView.getTag())) {
+                            mImageView.setImageBitmap(bitmap);
+                        }
+                        MemoryUtil.getMemory().put(getKey(imagePath), bitmap);
+                    } else {
+                        // 从网络上下载
+                        if (imagePath.equals(mImageView.getTag())) {
+                            downloadBitMap(imagePath, mImageView, errorResource, maxWidth, maxHeight);
+                        }
+                    }
                 }
-                MemoryUtil.getMemory().put(getKey(imagePath), bitmap);
-            } else {
-                // 从网络上下载
-                if (imagePath.equals(mImageView.getTag())) {
-                    downloadBitMap(imagePath, mImageView, errorResource, maxWidth, maxHeight);
-                }
-            }
+            });
         }
     }
 
     private static void downloadBitMap(final String imagePath, final ImageView mImageView, final int errorResource, int maxWidth, int maxHeight) {
         ImageRequest imageRequest = new ImageRequest(imagePath, new Response.Listener<Bitmap>() {
             @Override
-            public void onResponse(Bitmap bitmap) {
+            public void onResponse(final Bitmap bitmap) {
                 if (imagePath.equals(mImageView.getTag())) {
                     mImageView.setImageBitmap(bitmap);
                 }
                 MemoryUtil.getMemory().put(getKey(imagePath), bitmap);
-                BitmapUtil.saveBitmapToFile(bitmap, MemoryUtil.getSaveBitmapPath(getKey(imagePath)));
+                executorService.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        BitmapUtil.saveBitmapToFile(bitmap, MemoryUtil.getSaveBitmapPath(getKey(imagePath)));
+                    }
+                });
             }
         }, maxWidth, maxHeight, Bitmap.Config.RGB_565, new Response.ErrorListener() {
             @Override
